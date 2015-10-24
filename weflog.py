5fbf5c47ec8502ddccb76a3c49a3f9c2b1b3d342
amaljoyc <amaljoyc@gmail.com>
2015-10-24 23:12:16 +0200

    flask wtf forms

--- weflog.py
+++ weflog.py
@@ -1,17 +1,37 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my_secret_k3y'
+
 bootstrap = Bootstrap(app)
 m = Moment(app)
+
+
+class NameForm(Form):
+    name = StringField('Enter your name below', validators=[Required()])
+    submit = SubmitField('Submit')
 
 
 @app.route('/')
 def index():
     return render_template('index.html', current_time=datetime.utcnow())
+
+
+@app.route('/name-form', methods=['GET', 'POST'])
+def name_form():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name-form.html', form=form, name=name)
 
 
 @app.route('/<name>')
